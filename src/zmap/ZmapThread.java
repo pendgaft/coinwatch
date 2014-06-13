01e27388361f1a29c033aafddfa1813ad66635d9
pendgaft <schuch@cs.umn.edu>
2014-06-13 13:39:54 -0500

    finished zmap thread

--- src/zmap/ZmapThread.java
+++ src/zmap/ZmapThread.java
@@ -4,6 +4,8 @@ import java.net.*;
 import java.io.*;
 import java.util.*;
 
+import net.Constants;
+
 import data.Contact;
 
 public class ZmapThread implements Runnable {
@@ -21,6 +23,7 @@ public class ZmapThread implements Runnable {
 	}
 
 	@Override
+	@SuppressWarnings("unchecked")
 	public void run() {
 		try {
 			System.out.println("Starting set read.");
@@ -28,37 +31,52 @@ public class ZmapThread implements Runnable {
 			System.out.println("Finished set read: " + trialSet.size() + " elements");
 			Set<Contact> returnSet = new HashSet<Contact>();
 
-			// XXX testing
-			for (Contact tContact : trialSet) {
-				System.out.println(tContact.toString());
-			}
-			
 			/*
 			 * Write hosts to a file for zmap to white list
 			 */
 			String whiteFileName = FILE_DIR + this.conn.getInetAddress() + this.conn.getPort();
 			BufferedWriter whiteListFile = new BufferedWriter(new FileWriter(whiteFileName));
-			for(Contact tContact: trialSet){
-				if(tContact.getPort() == 8333){
-					whiteListFile.write(tContact.getIp().toString().split("/")[1] + "\n");
+			for (Contact tContact : trialSet) {
+				if (tContact.getPort() == Constants.DEFAULT_PORT) {
+					whiteListFile.write(tContact.getIp().toString().split("/")[1] + "/32\n");
 				}
 			}
 			whiteListFile.close();
 
-			//TODO zmap invoke
+			/*
+			 * Fork and wait on zmap
+			 */
 			System.out.println("Strating zmap");
 			Runtime rt = Runtime.getRuntime();
-			//Process childProcess = rt.exec("zmap");
-			//childProcess.waitFor();
+			Process childProcess = rt.exec("zmap -w " + whiteFileName + " -o " + whiteFileName
+					+ "-out -S 192.168.1.69 -P 5 -c 15 -i p34p1 -p 8333");
+			childProcess.waitFor();
 			System.out.println("Zmap finished");
 
-			// TODO read file into data structure
-
-			// XXX testing
-			returnSet.add(new Contact(InetAddress.getByName("5.6.7.8"), 124, 19, false));
+			/*
+			 * Read the results of the scan, load into set
+			 */
+			BufferedReader inBuffer = new BufferedReader(new FileReader(whiteFileName + "-out"));
+			// eat the first line as it is just the word "saddr"
+			inBuffer.readLine();
+			while (inBuffer.ready()) {
+				String readStr = inBuffer.readLine().trim();
+				if(readStr.length() > 0){
+					returnSet.add(new Contact(InetAddress.getByName(readStr), Constants.DEFAULT_PORT, 0 , false));
+				}
+			}
+			inBuffer.close();
 
 			System.out.println("Starting set write of " + returnSet.size() + " elements");
 			this.out.writeObject(returnSet);
+			
+			/*
+			 * Clean up the temp files
+			 */
+			File whiteFile = new File(whiteFileName);
+			whiteFile.delete();
+			whiteFile = new File(whiteFileName + "-out");
+			whiteFile.delete();
 		} catch (IOException | InterruptedException | ClassNotFoundException e) {
 			e.printStackTrace();
 		}
