1f120003e9a0496bb71123723cd3239af6d8782a
pendgaft <schuch@cs.umn.edu>
2014-07-10 13:02:11 -0500

    fixed contact updating in node object

--- src/net/Node.java
+++ src/net/Node.java
@@ -263,6 +263,10 @@ public class Node {
 		Addr incMessage = new Addr(advPayload);
 		List<Contact> incContacts = incMessage.getLearnedContacts();
 		synchronized (this.learnedContacts) {
+			/*
+			 * We need to remove the old contact objects first, adding in the new ones
+			 */
+			this.learnedContacts.removeAll(incContacts);
 			this.learnedContacts.addAll(incContacts);
 		}
 	}
